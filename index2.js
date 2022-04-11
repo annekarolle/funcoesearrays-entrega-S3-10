4eec97907d468fb6ce0b7f6c2461029fd92e215f
annekarolle <102996739+annekarolle@users.noreply.github.com>
2022-04-11 11:27:18 -0300

    atualizado

--- index2.js
+++ index2.js
@@ -1,13 +1,16 @@
 function positions (firstPlace, secondPlace, lastPlace) {
+
    let podio = [firstPlace, secondPlace, lastPlace];  
+
     if ("Daniel" === secondPlace){
       podio = [secondPlace, firstPlace, lastPlace];
     } 
-     if ("Daniel" === lastPlace){
+    if ("Daniel" === lastPlace){
       podio = [firstPlace, lastPlace, secondPlace];
     }
-   console.log(`1ª - Colocado ${podio[0]}, 2ª - Colocado ${podio[1]} , 3ª - Colocado ${podio[2]}`);  
 
+   console.log(`1ª - Colocado ${podio[0]}, 2ª - Colocado ${podio[1]} , 3ª - Colocado ${podio[2]}`);  //sem concatenação
+  //  console.log("1ª - Colocado" + " " + podio[0] + " " + "2ª - Colocado" + " " + podio[1] + " " + "3ª - Colocado" + " " + podio[2] ) //concatenado
 }
 
 positions("Manoel", "Daniel", "Rafael")
